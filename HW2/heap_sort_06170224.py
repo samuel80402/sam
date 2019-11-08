fff5137c6a9357358fc4938315c1d6d6a61d7641
samuel80402 <55569835+samuel80402@users.noreply.github.com>
2019-11-08 08:36:32 +0800

    Update heap_sort_06170224.py

--- HW2/heap_sort_06170224.py
+++ HW2/heap_sort_06170224.py
@@ -6,6 +6,10 @@
 
 class Solution(object):
     def heap_sort(self,nums):
+        """
+        :type nums: List[int] ex:[3,2,-4,6,4,2,19],[5,1,1,2,0,0]
+        :rtype: List[int] ex:[-4,2,2,3,4,6,19],[0,0,1,1,2,5]
+        """
         n = len(nums)
         for i in range(n-1,-1,-1):
             heapify(nums,i,n)
